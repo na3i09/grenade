a3f1d9f29d53d4de24e2fe0c5fc0f96e74651935
reign <reign@Big-OOO.local>
2026-02-10 14:00:00 -0600

    made score board only update on player connections and kills

--- score_list.gd
+++ score_list.gd
@@ -3,8 +3,12 @@ class_name ScoreList
 
 var score_dict: Dictionary[int,int] = {}
 
+var first_list: bool = true
 
 func _on_visibility_changed() -> void:
+	if first_list:
+		$"../../ScoreTracker".rpc("_request_score_board")
+		first_list = false
 	if visible:
 		$"../../ScoreTracker".request_score_board()
 
--- score_tracker.gd
+++ score_tracker.gd
@@ -4,16 +4,27 @@ class_name ScoreTracker
 
 var score_list: Dictionary[int,int] = {}
 
+func _ready() -> void:
+	if multiplayer.is_server():
+		multiplayer.peer_connected.connect(rebuild_score_list)
+
 func add_player(id: int) -> void:
 	if not score_list.has(id):
 		score_list[id] = 0
 
 func request_score_board() -> void:
-	rpc("_request_score_board")
+	$"../ScoreBoard/ScoreList".print_score(score_list)
 
 func remove_player(id: int) -> void:
 	if multiplayer.is_server():
 		score_list.erase(id)
+
+func rebuild_score_list() -> void:
+	rpc("_rebuild_score_list",score_list)
+
+@rpc("authority","call_remote","reliable",3)
+func _rebuild_score_list(_score_list: Dictionary) -> void:
+	score_list = _score_list
 
 @rpc("any_peer","call_local","reliable",3)
 func _request_score_board() -> void:
@@ -28,6 +39,8 @@ func recieve_score_board(_score_list: Dictionary) -> void:
 func add_kill(killer_id: int) -> void:
 	if score_list.has(killer_id):
 		score_list[killer_id] += 1
+		if multiplayer.is_server():
+			rebuild_score_list()
 
 func _on_player_kill(id: int) -> void:
 	add_kill(id)
