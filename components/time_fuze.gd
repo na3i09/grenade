4467f494c060e89cead1a50418a4bb12bc1c756a
reign <reign@Big-OOO.local>
2026-02-12 07:57:13 -0600

    changed time fuze to set timer wait time on arm instead of on ready

--- components/time_fuze.gd
+++ components/time_fuze.gd
@@ -3,13 +3,13 @@ class_name TimeFuze
 
 @export var fuze_time: float = 1.2
 
-@onready var timer: Timer = Helpers.create_internal_timer(self,fuze_time)
+@onready var timer: Timer = Helpers.create_internal_timer(self)
 
 func _ready() -> void:
 	timer.timeout.connect(_on_timer_timeout)
 
 func _arm() -> void:
-	timer.start()
+	timer.start(fuze_time)
 
 func _on_timer_timeout() -> void:
 	_detonate()
